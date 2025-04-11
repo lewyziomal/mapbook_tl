17ba22db6fd5d9e7e560e60c1a4ead7b4e8f399c
lewyziomal <tomaszlewandowski073@gmail.com>
2025-04-11 09:32:45 +0200

    upgraded

---
+++ utils/model.py
@@ -0,0 +1,6 @@
+users:list = [
+    {"name":"Tomasz","location":"Prudnik","posts":400},
+    {"name":"Adrian","location":"Toruń","posts":500},
+    {"name":"Bartek","location":"Giżycko","posts":700},
+    {"name":"Bernard","location":"Ełk","posts":200}
+]
