bd646c7d276451a7fa2aa38d3135c49eda02df23
lewyziomal <tomaszlewandowski073@gmail.com>
2025-04-16 11:13:11 +0200

    wip: new function: add user list

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ def main():
         print("0 - zakoncz program ")
         print("1 - pokaz co u znajomych")
         print("2 - dodaj nowego znajomego")
+        print("3 - usun znajomego")
         print("==============================")
         choice = input("wybierz opcje menu")
         if choice == "0": break
         if choice == "1": geu_user_info(info)
         if choice == "2": add_user(users)
-
+        if choice == "3": add_user(users)
 
 if __name__ == "__main__":
     main()
--- utils/controller.py
+++ utils/controller.py
@@ -1,8 +1,16 @@
-def get_user_info(users_data:list)->None:
+def get_user_info(users_data: list) -> None:
     for user in users_data:
         print(f"Twoj znajomy {user['name']}! z miejscowosci {user['location']} opublikował {user['posts']} postów")
 
- user_name=input("podaj imie nowego uzytkownika: "),
-    user_location=input("podaj lokalizacje znajomego"),
-    user_posts=input("oidaj liczbe postow"),
-    users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
+
+user_name = input("podaj imie nowego uzytkownika: "),
+user_location = input("podaj lokalizacje znajomego"),
+user_posts = input("oidaj liczbe postow"),
+users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
+
+
+def remove_user(useers_data: list) -> None:
+    user_name = imput("podaj imie  uzytkownika do usuniecia: ")
+    for user in users_data
+        if user['name'] == user_name:
+            users.remove(user)
