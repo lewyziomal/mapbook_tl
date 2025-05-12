c2de622af2c651208c5c60404b879b9948bbfb25
lewyziomal <tomaszlewandowski073@gmail.com>
2025-05-12 16:29:03 +0200

    wip: new function: update user list

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import choice
 
 from utils.model import users
-from utils.controller import get_user_info, add_users, remove_user
+from utils.controller import get_user_info, add_users, remove_user, update_user
 
 
 def main():
@@ -12,12 +12,14 @@ def main():
         print("1 - pokaż co u znajomych")
         print("2 - dodaj nowego znajomego")
         print("3 - usuń znajomego")
+        print("4 - aktualizuj dane znajomego")
         print('========================')
         choice = input("wybierz opcje menu: ")
         if  choice == "0": break
         if  choice == "1": get_user_info(users)
         if choice == "2": add_users(users)
         if choice == "3": remove_user(users)
+        if choice == "4": update_user(users)
 
 if __name__ == '__main__':
     main()
--- notatnik.py
+++ notatnik.py
@@ -1,14 +1,18 @@
 users:list = [ {"name":"Zuzia","location":"Radzyń_Podlaski","posts":700}]
+
 print(users)
 
-def remove_user(users_data: list)->None:
+def update_user(users_data: list)->None:
 
-    user_name=input("podaj imię użytkownika do usuniecia: ")
+    user_name=input("podaj imię użytkownika którego dane chcesz zaktualizować: ")
     for user in users_data:
         if user["name"] == user_name:
-            users_data.remove(user)
+            user["name"] = input("podaj nowe imię użytkownika: ")
+            user["location"] = input("podaj nową lokalizaję użytkownika: ")
+            user["posts"] = int(input("podaj nową liczbę postów użytkownika : "))
 
-remove_user(users)
+
+update_user(users)
 
 
 
